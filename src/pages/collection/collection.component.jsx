2028a5a7d9446ebb765e1ab359f144694dc65d5e
haider <haidertiger10@gmail.com>
2020-09-02 05:12:16 +0500

    first commit

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from "react";
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./collection.styles.scss";
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionContext from "../../context/collections/collections.context";
 
-import './collection.styles.scss';
+const CollectionPage = ({ match }) => {
+  // return (
+  //   <CollectionContext.Consumer>
+  //     {(collections) => {
+  //       const collection = collections[match.params.collectionId];
+  //       const { title, items } = collection;
+  //       return (
+  //         <div className="collection-page">
+  //           <h2 className="title">{title}</h2>
+  //           <div className="items">
+  //             {items.map((item) => (
+  //               <CollectionItem key={item.id} item={item} />
+  //             ))}
+  //           </div>
+  //         </div>
+  //       );
+  //     }}
+  //   </CollectionContext.Consumer>
+  // );
 
-const CollectionPage = ({ collection }) => {
+  const collections = useContext(CollectionContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -21,8 +40,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
